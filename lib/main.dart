1fa8efa070bff215be6e45228701b975f09d7629
Claudio Moreno <clauartur98@gmail.com>
2020-04-21 14:56:23 -0500

    feat: add appBar and Image content

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp((MaterialApp(
-    home: Center(
-      child: Text('Hola Mundo'),
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Center(child: Text('I Am Rich')),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+              image: NetworkImage(
+                  'https://codigobase.es/wp-content/uploads/2019/03/Google-flutter-logo.png')),
+        ),
+      ),
     ),
-  )));
+  );
 }
